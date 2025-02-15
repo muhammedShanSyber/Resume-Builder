667a1ae9241a10df7fc23623a87953e83bbfa68e
Mohammed Shan N <suharabeevi443@outlook.com>
2025-02-15 18:16:39 +0530

    Added some UI

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,6 @@ function App() {
     <div className="flex h-screen">
       <LeftSideBar />
       <CenterCanvas />
-
       <RightSideBar />
     </div>
   );
--- src/components/CenterCanvas.jsx
+++ src/components/CenterCanvas.jsx
@@ -2,10 +2,21 @@ function CenterCanvas() {
   return (
     <div className="flex-1 bg-white    justify-center items-center border-x">
       <div className="flex items-center justify-center ">
-        <h2 className="text-center text-2xl font-bold mb-2">
+        <h2 className="text-center text-2xl font-bold mb-2 absolute">
           Own Resume Builder
         </h2>
-        <button>Dark Mode</button>
+        <button className="border-2 bg-white border-blue-500 ml-96 p-2">Dark Mode</button>
+        <select className=" border border-gray-300 rounded p-2">
+          <option>English</option>
+          <option>Malayalam</option>
+          <option>Tamil</option>
+          <option>Hindi</option>
+          <option>Chinese</option>
+          <option>Arabic</option>
+          <option>Persian</option>
+          <option>Russian</option>
+
+        </select>
       </div>
 
       <div className="w-full h-4/5 bg-gray-50 shadow-md p-6 rounded-md">
